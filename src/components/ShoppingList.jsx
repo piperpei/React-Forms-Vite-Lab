167194ee5d0ad5cae7bd2e91f0a4cf4616ed19e9
piperpei <piperpei@yahoo.com>
2025-10-25 21:04:14 -0700

    Shopping Forms Lab Complete

--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -3,23 +3,43 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+function ShoppingList({ items = [] }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search, setSearch] = useState(""); 
+  const [itemList, setItemList] = useState(items);
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  function handleSearchChange(event) {
+    setSearch(event.target.value); 
+  }
 
-    return item.category === selectedCategory;
+  function handleAddItem(newItem) {
+    setItemList([...itemList, newItem]);
+  }
+
+  const itemsToDisplay = itemList.filter((item) => {
+    
+    if (selectedCategory !== "All" && item.category !== selectedCategory) {
+      return false;
+    }
+    
+    if (search && !item.name.toLowerCase().includes(search.toLowerCase())) {
+      return false;
+    }
+    return true;
   });
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={handleAddItem} />
+      <Filter
+        onCategoryChange={handleCategoryChange}
+        onSearchChange={handleSearchChange} 
+        search={search} 
+      />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
